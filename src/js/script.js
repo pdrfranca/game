99c58c4e07d799672f7b893c7cd24071390bc222
Pedro França <pfrutopro@gmail.com>
2024-09-25 12:24:03 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+(function(d){
+    var s = d.createElement("script");
+    s.setAttribute("data-account", "INSIRA_SEU_CÓDIGO_USERWAY_AQUI");
+    s.setAttribute("src", "https://cdn.userway.org/widget.js");
+    (d.body || d.head).appendChild(s);
+})(document);
